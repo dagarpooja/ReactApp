9df6fcf8cad7092ae08cfc4ce1ac5d2a15f78f90
dagarpooja <pdagar67@gmail.com>
2023-01-11 15:27:08 +0530

    login form

--- demo/src/components/About.jsx
+++ demo/src/components/About.jsx
@@ -2,7 +2,25 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>hello,I am About page</div>
+    // <div>hello,I am About page</div>
+    <div className="form">
+     <form>
+       <div className="input-container">
+         <label>Username </label>
+         <input type="text" name="uname" required />
+         
+       </div>
+       <div className="input-container">
+         <label>Password </label>
+         <input type="password" name="pass" required />
+         
+       </div>
+       <div className="button-container">
+         <input type="submit" />
+       </div>
+     </form>
+   </div>
+    
   )
 }
 
